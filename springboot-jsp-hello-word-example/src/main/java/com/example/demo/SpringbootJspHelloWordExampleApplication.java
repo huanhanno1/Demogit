84db48b7830fe54caa143c2bc9a3972f0c1b7a7c
Nam Nguyễn <hoainam65877@gmail.com>
2025-05-20 12:44:34 +0700

    Add files via upload

---
+++ springboot-jsp-hello-word-example/src/main/java/com/example/demo/SpringbootJspHelloWordExampleApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJspHelloWordExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJspHelloWordExampleApplication.class, args);
+	}
+
+}
+	
